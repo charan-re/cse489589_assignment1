4450e2481a4ae843629ef1c946d0b89f814ae1b8
charan-re <70591002+charan-re@users.noreply.github.com>
2020-09-23 18:44:32 -0400

    Add files via upload

---
+++ charanre/include/global.h
@@ -0,0 +1,17 @@
+#ifndef GLOBAL_H_
+#define GLOBAL_H_
+
+#define HOSTNAME_LEN 128
+#define PATH_LEN 256
+#define MAX_CONNECTIONS 5
+#endif
+
+
+#define BACKLOG 5
+#define STDIN 0
+#define TRUE 1
+#define CMD_SIZE 100
+#define BUFFER_SIZE 256
+
+
+
